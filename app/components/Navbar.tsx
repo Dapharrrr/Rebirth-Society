b259a46bf236702af2bde52280e41bc5c1b3afee
BastienU <bastien.u04@gmail.com>
2025-09-03 12:21:09 +0200

    Update responsive on navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,20 @@
-import Image from 'next/image';
-import styles from './navbar.module.scss';
-
+import Image from "next/image";
+import styles from "./navbar.module.scss";
 
 const Navbar: React.FC = () => (
   <nav className={styles.navigation} aria-label="Main navigation">
-    <Image src="/globe.svg" alt="Logo of the school" width={50} height={50} />
-    <h1 className={styles.title}>Rebirth Society School</h1>
-
-    <div className={styles.navContainer}>
-      <div className={styles.navLinks}>
-        <a className={styles.navLink} href="#learn">Courses</a>
-        <a className={styles.navLink} href="#examples">About</a>
-        <a className={styles.navLink} href="#deploy">Contact</a>
-      </div>
-
-      <a className={styles.login} href="#login">Login</a>
+    <div className={styles.navLeft}>
+      <a href="#"><Image src="/images/logo.jpg" alt="Logo of the school" width={50} height={50} /></a>
+      <h1 className={styles.title}>Rebirth Society</h1>
     </div>
+
+    <div className={styles.navCenter}>
+      <a className={styles.navLink} href="#learn">Courses</a>
+      <a className={styles.navLink} href="#examples">About</a>
+      <a className={styles.navLink} href="#deploy">Contact</a>
+    </div>
+
+    <a className={styles.login} href="#login">Login</a>
   </nav>
 );
 
